429e1a414279ff194bfdfdc82af27dcb712785cf
NeiRo21 <neiro21@gmail.com>
2025-11-11 18:08:33 +0000

    chore: Use stricter Eslint configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,8 @@ import tseslint from 'typescript-eslint';
 
 export default defineConfig(
     eslint.configs.recommended,
-    tseslint.configs.recommended,
+    tseslint.configs.strict,
+    tseslint.configs.stylistic,
     {
 		// Note: there should be no other properties in this object
 		ignores: ["dist/", "node_modules/", ".vscode/", ".idea/"],
