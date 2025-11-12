9821ac9c3b569976b8b82266085484eb76c01811
NeiRo21 <neiro21@gmail.com>
2025-11-12 16:57:28 +0000

    chore: Upgrade ESLint
    * Fixed linter findings.
    * Added Node.js version restriction.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,94 @@
-// @ts-check
+// MIT License
+//
+// Copyright (c) 2025 NeiRo21
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
 
-import eslint from "@eslint/js";
-import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+import markdown from "@eslint/markdown";
+import { defineConfig, globalIgnores } from "eslint/config";
 import tseslint from "typescript-eslint";
+import prettier from "eslint-plugin-prettier/recommended";
+import importPlugin from "eslint-plugin-import";
+import globals from "globals";
+import headers from "eslint-plugin-headers";
 
 export default defineConfig(
-  eslint.configs.recommended,
+  // JS
+  {
+    ...js.configs.recommended,
+    files: ["**/*.js", "**/*.mjs"],
+  },
+  // TS
   tseslint.configs.strict,
   tseslint.configs.stylistic,
+  // Import
   {
-    // Note: there should be no other properties in this object
-    ignores: ["dist/", "node_modules/", ".vscode/", ".idea/"],
+    extends: [
+      importPlugin.flatConfigs.recommended,
+      importPlugin.flatConfigs.typescript,
+    ],
+    settings: {
+      "import/resolver": {
+        typescript: true,
+        node: true,
+      },
+    },
+    files: ["**/*.ts", "**/*.tsx"],
   },
+  // Globals
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  // Markdown
+  {
+    files: ["**/*.md"],
+    plugins: { markdown },
+    language: "markdown/gfm",
+    extends: ["markdown/recommended"],
+  },
+  // License headers
+  {
+    plugins: {
+      headers,
+    },
+    rules: {
+      "headers/header-format": [
+        "error",
+        {
+          source: "file",
+          path: "LICENSE",
+          style: "line",
+          blockSuffix: "\n",
+        },
+      ],
+    },
+  },
+  // Ignore
+  globalIgnores(["addon/", "dist/", "node_modules/", ".vscode/", ".idea/"]),
+  // Prettier
+  prettier,
 );
